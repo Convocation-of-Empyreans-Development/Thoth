97b77ed56d258568e58d9167eab94a2f2366bfbb
James Dietz <james-dietz@users.noreply.github.com>
2020-04-27 03:29:45 +0100

    Refactor bot login test
    Store token in variable, then login using the token.
    Force the assertion to happen once the bot's login is completed.

--- test/test.js
+++ test/test.js
@@ -7,14 +7,18 @@ const DISCORD_BOT_USERNAME = "Thoth#7445";
 describe("Discord bot", function() {
     describe("#initialize", function() {
         it("should log in using the provided token", function() {
+            let token;
             if (process.env.GITHUB_ACTIONS) {
-                bot.login(process.env.DISCORD_BOT_TOKEN);
+                token = process.env.DISCORD_BOT_TOKEN;
             } else {
                 var AuthDetails = require("../../secret/discordSecret.json");
-                bot.login(AuthDetails.bot_token);
+                token = AuthDetails.bot_token;
             }
-            assert.equal(bot.user.tag, DISCORD_BOT_USERNAME);
-            bot.destroy();
+            bot.login(token)
+                .then(() => {
+                    assert.equal(bot.user.tag, DISCORD_BOT_USERNAME);
+                    bot.destroy();
+                })
         })
     })
 })
